788ade8c62038f7d3f4880cea5077d225a93df41
Ana Isabel Carvalho <ana.isabelc@gmail.com>
2014-04-16 17:54:30 +0100

    Organized homepage list in two sections: portfolio and news.

--- manufactura-timeline/index.php
+++ manufactura-timeline/index.php
@@ -8,15 +8,16 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-		
-      <?php
+
+		<h4 class="row">Portfolio</h4>
+ 		<?php
 
         $blogtime = date('Y');
         $prev_year = '';
 				$prev_month = '';
 
         $args = array(
-                 'category' => 'portfolio',
+                 'category_name' => 'portfolio',
          					'posts_per_page' => 50
         );
       
@@ -28,7 +29,43 @@ get_header(); ?>
 				?>
 
       <article id="post-<?php the_ID(); ?>" <?php post_class('row'); ?>>
+					<?php if(get_the_time('M') != $prev_month || get_the_time('Y') != $prev_year && get_the_time('Y') == $prev_limit_year) {
+        		echo "<span class=\"date-title columns large-1 tight\">".get_the_time('M Y')."</span>\n\n";
+					}
+					?>
+        <h2 class="entry-title columns large-6">
+          <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+        </h2>
+        <footer class="entry-summary columns large-5 tight">
+          <?php the_excerpt(); ?>
+        </footer><!-- .entry-meta -->
+      </article><!-- #post -->
+      <?php
+          $prev_month = get_the_time('Y');
+          $prev_year = get_the_time('Y');
+        }
+		  ?>
 
+		<h4 class="row">Other</h4>	
+      <?php
+
+        $blogtime = date('Y');
+        $prev_year = '';
+				$prev_month = '';
+
+        $args = array(
+                 'category_name' => 'news',
+         					'posts_per_page' => 50
+        );
+      
+        $postsbymonth = new WP_Query($args);
+        while($postsbymonth->have_posts()) {
+          $postsbymonth->the_post();
+          $do_not_duplicate = $post->ID;
+
+				?>
+
+      <article id="post-<?php the_ID(); ?>" <?php post_class('row'); ?>>
 					<?php if(get_the_time('M') != $prev_month || get_the_time('Y') != $prev_year && get_the_time('Y') == $prev_limit_year) {
         		echo "<span class=\"date-title columns large-1 tight\">".get_the_time('M Y')."</span>\n\n";
 					}
